4a78d9e83f089c30670ec2695ef7947a138cfbd4
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-08 17:06:18 +0900

    Dynamic property values and text

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import MyComponent from './components/MyComponent';
+const enabled = false;
+const text = 'A Button';
+const placeholder = 'input value...';
+const size = 50;
 
 render((
-    <MyComponent>
-        <MyComponent.First />
-        <MyComponent.Second />
-    </MyComponent>
+    <section>
+        <button disabled={!enabled}>{text}</button>
+        <input placeholder={placeholder} size={size}/>
+    </section>
 ),
     document.querySelector('.container')
 );
