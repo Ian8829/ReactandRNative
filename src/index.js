ca3ea747e6f893db380c9e6bdf8c7b8438fe0b7d
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-08 17:15:46 +0900

    Mapping collections to elements

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const enabled = false;
-const text = 'A Button';
-const placeholder = 'input value...';
-const size = 50;
+// An array that we want to render as a list..
+const array = [
+    'First',
+    'Second',
+    'Third'
+];
+
+// An object that we want to render as a list...
+const object = {
+    first: 1,
+    second: 2,
+    third: 3
+};
 
 render((
     <section>
-        <button disabled={!enabled}>{text}</button>
-        <input placeholder={placeholder} size={size}/>
+        <h1>Array</h1>
+        <ul>
+            {array.map(i => (
+                <li key={i}>{i}</li>
+            ))}
+        </ul>
+
+        <h1>Object</h1>
+        <ul>
+            {Object.keys(object).map(i => (
+                <li key={i}>
+                    <strong>{i}: </strong>{object[i]}
+                </li>
+            ))}
+        </ul>
     </section>
-),
+    ),
     document.querySelector('.container')
 );
