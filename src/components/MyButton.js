1b20f0c00e2c0a2d6476794a51a568b6dcde2c7b
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-09 17:05:27 +0900

    Setting property values2

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 
 export default class MyButton extends Component {
-    static defaultProps = {
-        disabled: false,
-        text: 'Props Btn'
-    };
-
     render() {
         const { disabled, text} = this.props;
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
+import { render as renderJSX } from 'react-dom';
 import MyButton from "./components/MyButton";
+import MyList from "./components/MyList";
 
-render(
-    <MyButton/>,
+const appState = {
+    disabled: false,
+    text: 'My Button',
+    items: [
+        'First',
+        'Second',
+        'Third'
+    ]
+};
+
+function render(props) {
+    renderJSX((
+        <main>
+            <MyButton
+                disabled={props.disabled}
+                text={props.text}
+            />
+            <MyList
+                items={props.items}
+            />
+        </main>
+    ),
     document.querySelector('.container')
-);
+    );
+}
+
+render(appState);
+
+setTimeout(() => {
+    appState.disabled = true;
+    appState.items.splice(0, 2);
+    render(appState);
+}, 2000);
+
+setTimeout(() => {
+    appState.items.push('Fourth...');
+    render(appState);
+}, 4000);
