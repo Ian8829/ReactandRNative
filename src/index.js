0352b2980bd5b053cff6db01f70fdd9bd119ba07
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-13 10:51:38 +0900

    Auto-binding context

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import MyContainer from "./components/MyContainer";
+// import React, { Component } from 'react';
+// import { render } from 'react-dom';
+// import MyContainer from "./components/MyContainer";
+//
+// render(
+//   <MyContainer/>,
+//   document.querySelector('.container')
+// );
 
-render(
-  <MyContainer/>,
-  document.querySelector('.container')
-);
+import React from 'react';
+
+export default React.createClass({
+  onClick() {
+    console.log('clicked',
+      `${this.props.children}`);
+  },
+
+  render() {
+    return(
+      <button onClick={this.onClick}>
+        {this.props.children}
+      </button>
+    );
+  }
+});
