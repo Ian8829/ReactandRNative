00bb52f391ffb87eccf019b5e8ad9c4dde203f6a
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-14 12:10:11 +0900

    Initial state and state helpers

--- src/components/MyFeature.js
+++ src/components/MyFeature.js
@@ -1,7 +1,51 @@
 import React, { Component } from 'react';
+// Utility for constructing unique IDs
+import cuid from 'cuid';
+// For building immutable component state
+import { fromJS } from 'immutable';
 
 export default class MyFeature extends Component {
+  state = {
+    data: fromJS({
+      articles: [
+        {
+          id: cuid(),
+          title: 'Article 1',
+          summary: 'Article 1 Summary',
+          display: 'none'
+        },
+        {
+          id: cuid(),
+          title: 'Article 2',
+          summary: 'Article 2 Summary',
+          display: 'none'
+        },
+        {
+          id: cuid(),
+          title: 'Article 3',
+          summary: 'Article 3 Summary',
+          display: 'none'
+        },{
+          id: cuid(),
+          title: 'Article 4',
+          summary: 'Article 4 Summary',
+          display: 'none'
+        },
+      ],
+      title: '',
+      summary: ''
+    })
+  };
 
+  // Getter for 'Immutable.js" state data..
+  get data() {
+    return this.state.data;
+  }
+
+  // Setter for 'Immutable.js" state data..
+  set data(data) {
+    this.setState({data})
+  }
 
   render() {
     const { articles, title, summary }
